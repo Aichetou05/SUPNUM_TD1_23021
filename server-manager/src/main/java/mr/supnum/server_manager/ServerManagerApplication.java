5ab5045a891eb15c6bec101aa5565560fce72fad
Aichetou05 <23021@supnum.mr>
2025-11-20 22:24:15 +0100

    intialiser le projet

---
+++ server-manager/src/main/java/mr/supnum/server_manager/ServerManagerApplication.java
@@ -0,0 +1,13 @@
+package mr.supnum.server_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerManagerApplication.class, args);
+	}
+
+}
---
+++ server-manager/src/test/java/mr/supnum/server_manager/ServerManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package mr.supnum.server_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
