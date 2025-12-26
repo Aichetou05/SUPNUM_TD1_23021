22f26223b5d9a737a99c5c95a3645bf07ad4071c
Aichetou05 <23021@supnum.mr>
2025-12-26 15:13:46 +0100

    vide

--- server-manager/src/main/java/mr/supnum/server_manager/exception/ServerDeletionException.java
+++
@@ -1,7 +0,0 @@
-package mr.supnum.server_manager.exception;
-
-public class ServerDeletionException extends RuntimeException {
-    public ServerDeletionException(String message) {
-        super(message);
-    }
-}
--- server-manager/src/main/java/mr/supnum/server_manager/exception/ServerNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package mr.supnum.server_manager.exception;
-
-public class ServerNotFoundException extends RuntimeException {
-    public ServerNotFoundException(Long id) {
-        super("Server with id " + id + " not found");
-    }
-}
