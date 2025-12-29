16e5b25fdd5579d671f648c4450d21aa2c4655e0
Aichetou05 <23021@supnum.mr>
2025-12-29 13:32:27 +0100

    add the controller

--- server-manager-middle/src/main/java/com/supnum/server_manager_middle/controller/MiddleController.java
+++ server-manager-middle/src/main/java/com/supnum/server_manager_middle/controller/MiddleController.java
@@ -1,23 +1,50 @@
 package com.supnum.server_manager_middle.controller;
 
+import com.supnum.server_manager_middle.clients.ConsumateurClient;
+import com.supnum.server_manager_middle.dto.GetServerStatusResponse;
+import com.supnum.server_manager_middle.dto.Server;
+import com.supnum.server_manager_middle.dto.StartServerResponse;
+import com.supnum.server_manager_middle.dto.StopServerResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.supnum.server_manager_middle.soap.ServerSoapClient;
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/servers")
+@RequestMapping("/api/client/servers")
 public class MiddleController {
 
-    private final ServerSoapClient soapClient;
+    private final ConsumateurClient consumateurClient;
 
-    public MiddleController(ServerSoapClient soapClient) {
-        this.soapClient = soapClient;
+    public MiddleController(ConsumateurClient consumateurClient) {
+        this.consumateurClient = consumateurClient;
     }
 
     @GetMapping
-    public String getServers() {
-        return "REST → SOAP communication works";
+    public ResponseEntity<List<Server>> getAllServers() {
+        List<Server> servers = consumateurClient.getAllServers();
+        return ResponseEntity.ok(servers);
+    }
+
+    @GetMapping("/{id}/status")
+    public ResponseEntity<GetServerStatusResponse> getServerStatus(@PathVariable Long id) {
+        GetServerStatusResponse response = consumateurClient.getServerStatus(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/{id}/start")
+    public ResponseEntity<StartServerResponse> startServer(@PathVariable Long id) {
+        StartServerResponse response = consumateurClient.startServer(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/{id}/stop")
+    public ResponseEntity<StopServerResponse> stopServer(@PathVariable Long id) {
+        StopServerResponse response = consumateurClient.stopServer(id);
+        return ResponseEntity.ok(response);
     }
 }
