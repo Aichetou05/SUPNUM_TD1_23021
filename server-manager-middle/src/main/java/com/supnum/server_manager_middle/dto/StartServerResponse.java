56035c25dabe33f1ec0dc52c00d8283e4ff22ea6
Aichetou05 <23021@supnum.mr>
2025-12-29 13:27:18 +0100

    add the dtos

---
+++ server-manager-middle/src/main/java/com/supnum/server_manager_middle/dto/StartServerResponse.java
@@ -0,0 +1,15 @@
+package com.supnum.server_manager_middle.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class StartServerResponse {
+    private Server server;
+}
